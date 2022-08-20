d546e3c32ba0fc32b85040409e581ff845e4600e
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2022-08-20 20:46:22 +0200

    Initialize project

---
+++ src/test/java/com/fakeuserdatagenerator/fakeuserdatagenerator/FakeUserDataGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakeuserdatagenerator.fakeuserdatagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeUserDataGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
